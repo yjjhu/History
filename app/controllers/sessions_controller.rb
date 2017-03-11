ff49711445fb14886434b1f78e071a21375e9c25
yjjhu <yjjhu@ym.edu.tw>
2017-03-11 23:05:35 +0800

    3st

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     #驗證使用者，若成功，就建立一個 session，把 user_id 放入 session hash
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_path
+      redirect_to events_path
     else
       redirect_to login_path
     end
@@ -18,6 +18,6 @@ class SessionsController < ApplicationController
   def destroy
     #登出畫面使用，刪除 session hash 裡面的 user_id
     session[:user_id] = nil
-    redirect_to root_path
+    redirect_to events_path
   end
 end
